21600391e998580a1e2d8c44e67a257830b8de69
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-10-11 01:12:50 -0500

    change pdf name

--- HW2/makePdf.ps1
+++ HW2/makePdf.ps1
@@ -1,5 +1,5 @@
-$proc = Start-Process -PassThru -NoNewWindow -FilePath "pandoc" -ArgumentList "HW2.md", "-o Task1.pdf"
+$proc = Start-Process -PassThru -NoNewWindow -FilePath "pandoc" -ArgumentList "HW2.md", "-o HW2.pdf"
 $proc.WaitForExit();
 # if ($proc.ExitCode -eq 0) {
-Start-Process ((Resolve-Path "Task1.pdf").Path)
+Start-Process ((Resolve-Path "HW2.pdf").Path)
 # }
