d98ab0807d82b2b108e48794630d5c5f777c35bb
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-10-11 00:31:09 -0500

    update

--- HW2/makePdf.ps1
+++ HW2/makePdf.ps1
@@ -1,2 +1,5 @@
-pandoc HW2.md -o Task1.pdf
+$proc = Start-Process -PassThru -NoNewWindow -FilePath "pandoc" -ArgumentList "HW2.md", "-o Task1.pdf"
+$proc.WaitForExit();
+# if ($proc.ExitCode -eq 0) {
 Start-Process ((Resolve-Path "Task1.pdf").Path)
+# }
