f3942b2a8f2175fbe32f4b97142048cf72a0d647
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-10-27 11:34:22 -0500

    basic test

--- HW3/memLeakTest.c
+++ HW3/memLeakTest.c
@@ -1,5 +1,10 @@
-#include "stdlib.h"
 #include "stdio.h"
+#include "stdlib.h"
+#include <unistd.h>
 
 int main() {
+    printf("Allocing\n");
+    void *ptr = malloc(512);
+    printf("Exiting\n");
+    sleep(1);
 }
